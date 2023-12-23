1cb0026654a5980b0216ffd9bd186907f558547c
Alex Andreev <sesh_1.0@niuitmo.ru>
2023-12-23 13:14:03 +0300

    some

--- fixed_set.h
+++ fixed_set.h
@@ -1,209 +1,204 @@
 #include <vector>
-#include <functional>
 #include <random>
-#include <cassert>
 #include <optional>
 #include <string>
-
+#include <algorithm>
 
 class BadHashFunctionException : public std::exception {
+    std::string error_message_;
+
 public:
-    std::string errorMessage;
-    explicit BadHashFunctionException(const std::string& message) : errorMessage(message) {}
+    explicit BadHashFunctionException(const std::string& message) : error_message_(message) {
+    }
 
     const char* what() const noexcept override {
-        return errorMessage.c_str();
+        return error_message_.c_str();
     }
 };
 
-class HashFunction {
+class LinearHashFunction {
+    int coefficien_;
+    int bias_;
+    int k_prime_;
+    int64_t not_negative_value_;
+    static const int64_t kMaxValue = 1000000000;
+
 public:
-    virtual size_t GetHash(int value) const = 0;
-    virtual ~HashFunction() {}
-};
+    LinearHashFunction(int coefficien, int bias, int k_prime) :
+    coefficien_(coefficien), bias_(bias), k_prime_(k_prime),
+    not_negative_value_(k_prime_ * kMaxValue) {
+    }
 
-class LinearHashFunction : public HashFunction {
-public:
-    int m_coefficien;
-    int m_bias;
-    static const size_t kPrime = 1000000021;
-    LinearHashFunction(int coefficien, int bias) : m_coefficien(coefficien), m_bias(bias) {}
-
-    ~LinearHashFunction() override = default;
-
-    size_t GetHash(int value) const override {
-        return static_cast<size_t>((1LL * value * m_coefficien + m_bias) % kPrime);
+    int GetHash(int value) const {
+        int ans =  (static_cast<int64_t>(value) * coefficien_ +
+            bias_ + not_negative_value_) % k_prime_;
+        if (ans < 0) {
+            throw BadHashFunctionException("ans < 0");
+        }
+        return ans;
     }
 };
 
-class GenerateHashFunction {
+class GenerateLinearHashFunction {
+    std::mt19937 generator_;
+
 public:
-    virtual HashFunction* Generate() = 0;
-};
+    static const int kPrime = 1000000021;
 
-class GenerateLinearHashFunction : public GenerateHashFunction {
-public:
-    std::mt19937 gen;
-    static const size_t kPrime = 1000000021;
+    GenerateLinearHashFunction() : generator_(std::random_device()()) {
+    }
 
-    GenerateLinearHashFunction() : gen(std::random_device()()) {}
-
-    HashFunction* Generate() override {
-        std::uniform_int_distribution<int> distribution(1, kPrime);
-        int coefficien = distribution(gen) % (kPrime - 1) + 1;
-        int bias = distribution(gen) % kPrime;
-        return new LinearHashFunction(coefficien, bias);
+    LinearHashFunction Generate() {
+        std::uniform_int_distribution<int> distribution_coefficien(1, kPrime - 1);
+        std::uniform_int_distribution<int> distribution_bias(0, kPrime - 1);
+        int coefficien = distribution_coefficien(generator_);
+        int bias = distribution_bias(generator_);
+        return LinearHashFunction(coefficien, bias, kPrime);
     }
 };
 
 
 class FixedSet {
-private:
-    static HashFunction* GetHashFunction(size_t cnt_buckets,
-                                         const std::vector<int>&numbers,
-                                         std::function<size_t(int)> fun,
-                                         int criteria,
-                                         GenerateLinearHashFunction* generator) {
-        int cnt_run = 0;
-        while (cnt_run < 10) {
-            cnt_run++;
-            std::vector<int> lens(cnt_buckets, 0);
-            auto* hash = generator->Generate();
-            for (int v: numbers) {
-                lens[hash->GetHash(v) % cnt_buckets] += 1;
-            }
-            int hu = 0;
-            for (int x: lens) {
-                hu += fun(x);
-            }
-            if (hu <= criteria) {
-                return hash;
-            }
-        }
-        throw BadHashFunctionException("Bad hash function");
-    }
-
-    static std::vector<std::vector<int>> Split(
-        const std::vector<int>&numbers,
-        HashFunction* hash,
-        size_t cnt_buckets) {
-        std::vector<std::vector<int>> buckets(cnt_buckets, std::vector<int>());
-        for (int v: numbers) {
-            size_t index = hash->GetHash(v) % cnt_buckets;
-            buckets[index].push_back(v);
-        }
-        return buckets;
-    }
-
     class InnerSet {
-    public:
-        GenerateLinearHashFunction* generator;
-        HashFunction* m_hash;
-        std::vector<std::optional<int>> m_data;
-        size_t m_cnt_buckets;
-
-        explicit InnerSet(GenerateLinearHashFunction* generator) :
-            generator(generator),  m_hash(nullptr) {}
-
-        ~InnerSet() {
-            delete m_hash;
-        }
-
-        InnerSet(InnerSet&& other) noexcept
-            : generator(other.generator), m_hash(other.m_hash),
-              m_data(std::move(other.m_data)), m_cnt_buckets(other.m_cnt_buckets) {
-            other.generator = nullptr;
-            other.m_hash = nullptr;
-            other.m_cnt_buckets = 0;
-        }
+        std::optional<LinearHashFunction> hash_;
+        std::vector<std::optional<int>> data_;
 
         static std::vector<std::optional<int>> Split(
-            const std::vector<int>&numbers,
-            HashFunction* hash,
-            size_t cnt_buckets) {
-            std::vector<std::optional<int>> buckets(cnt_buckets);
+                const std::vector<int>& numbers,
+                LinearHashFunction& hash,
+                int cnt_buckets) {
+                std::vector<std::optional<int>> buckets(cnt_buckets);
             for (int v: numbers) {
-                size_t index = hash->GetHash(v) % cnt_buckets;
+                int index = hash.GetHash(v) % cnt_buckets;
                 buckets[index].emplace(v);
             }
             return buckets;
         }
 
-        void Initialize(const std::vector<int>&numbers) {
-            delete m_hash;
-            size_t cnt_number = numbers.size();
-            m_cnt_buckets = cnt_number * cnt_number + 1;
-            assert(m_cnt_buckets != 0);
+    public:
+        InnerSet() = default;
+
+        void Initialize(const std::vector<int>& numbers, GenerateLinearHashFunction& generator) {
+            int cnt_number = numbers.size();
+            int cnt_buckets = cnt_number * cnt_number;
+            if (cnt_buckets == 0) {
+                return;
+            }
             // X % 0 - is UB
-            m_hash = GetHashFunction(
-                m_cnt_buckets, numbers,
-                [](int value) {
-                    return (value <= 1) ? 0 : value;
-                }, 0, generator);
-            m_data = Split(
+            hash_ = GetHashFunction(
+                cnt_buckets, numbers,
+                [](const std::vector<int>& lens) {
+                    return std::all_of(lens.begin(), lens.end(),
+                        [](int element) { return element <= 1; });
+                }, generator);
+            data_ = Split(
                 numbers,
-                m_hash,
-                m_cnt_buckets);
+                hash_.value(),
+                cnt_buckets);
         }
 
         bool Contains(int number) const {
-            assert(m_cnt_buckets != 0);
-            size_t index = m_hash->GetHash(number) % m_cnt_buckets;
-            if (m_data[index].has_value()) {
-                return m_data[index].value() == number;
+            int cnt_buckets = data_.size();
+            if (cnt_buckets == 0) {
+                return false;
+            }
+            int index = hash_.value().GetHash(number) % cnt_buckets;
+            if (data_[index].has_value()) {
+                return data_[index].value() == number;
             } else {
                 return false;
             }
         }
     };
 
+    std::optional<LinearHashFunction> hash_;
+    std::vector<InnerSet> data_;
+    static const int kMaxCountRun = 1000;
+
+    template<typename Predicate>
+    static LinearHashFunction GetHashFunction(int cnt_buckets,
+                                              const std::vector<int>& numbers,
+                                              Predicate predicat,
+                                              GenerateLinearHashFunction& generator) {
+        int count_run = 0;
+        while (count_run < kMaxCountRun) {
+            count_run++;
+            std::vector<int> lens(cnt_buckets, 0);
+            auto hash = generator.Generate();
+            for (int v: numbers) {
+                lens[hash.GetHash(v) % cnt_buckets] += 1;
+            }
+            if (predicat(lens)) {
+                return hash;
+            }
+        }
+        throw BadHashFunctionException("Bad hash function");
+    }
+
+    static int SquereSum(const std::vector<int>& lens) {
+        int hu = 0;
+        for (int x: lens) {
+            hu += x * x;
+        }
+        return hu;
+    }
+
+    static std::vector<std::vector<int>> Split(
+        const std::vector<int>& numbers,
+        LinearHashFunction& hash,
+        int cnt_buckets) {
+        std::vector<std::vector<int>> buckets(cnt_buckets, std::vector<int>());
+        for (int v: numbers) {
+            int index = hash.GetHash(v) % cnt_buckets;
+            buckets[index].push_back(v);
+        }
+        return buckets;
+    }
+
     std::vector<InnerSet> InitBuckets(
-        const std::vector<std::vector<int>>&buckets) {
+        const std::vector<std::vector<int>>& buckets, GenerateLinearHashFunction& generator) {
         std::vector<InnerSet> data;
-        for (const auto&bucket: buckets) {
-            data.emplace_back(&generator);
-            data.back().Initialize(bucket);
+        for (const auto& bucket: buckets) {
+            data.emplace_back();
+            data.back().Initialize(bucket, generator);
         }
         return data;
     }
 
 public:
-    HashFunction* m_hash;
-    std::vector<InnerSet> m_data;
-    size_t m_cnt_buckets;
-    GenerateLinearHashFunction generator;
+    FixedSet() = default;
 
-    FixedSet(): m_hash(nullptr), m_cnt_buckets(0), generator(GenerateLinearHashFunction())  {
-    }
+    void Initialize(const std::vector<int>& numbers) {
+        GenerateLinearHashFunction generator = GenerateLinearHashFunction();
 
-    ~FixedSet() {
-        delete m_hash;
-    }
+        data_.clear();
 
-    void Initialize(const std::vector<int>&numbers) {
-        delete m_hash;
-        m_data.clear();
-        size_t cnt_number = numbers.size();
-        m_cnt_buckets = cnt_number + 1;
-        assert(m_cnt_buckets != 0);
+        int cnt_number = numbers.size();
+        int cnt_buckets = cnt_number;
+        if (cnt_buckets == 0) {
+            return;
+        }
         // X % 0 - is UB
-        m_hash = GetHashFunction(
-            m_cnt_buckets,
+        hash_ = GetHashFunction(
+            cnt_buckets,
             numbers,
-            [](int value) {
-                return value * value;
-            }, 2 * cnt_number,
-            &generator);
+            [](const std::vector<int>& lens) {
+                return SquereSum(lens) <= static_cast<int>(2 * lens.size());
+            },
+            generator);
         auto buckets = Split(
             numbers,
-            m_hash,
-            m_cnt_buckets);
-        m_data = InitBuckets(buckets);
+            hash_.value(),
+            cnt_buckets);
+        data_ = InitBuckets(buckets, generator);
     }
 
     bool Contains(int number) const {
-        assert(m_cnt_buckets != 0);
-        size_t first = m_hash->GetHash(number) % m_cnt_buckets;
-        return m_data[first].Contains(number);
+        if (data_.empty()) {
+            return false;
+        }
+        int cnt_buckets = data_.size();
+        int first = hash_.value().GetHash(number) % cnt_buckets;
+        return data_[first].Contains(number);
     }
 };
